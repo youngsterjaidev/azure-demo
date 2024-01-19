3ee3b85c6bd02a020f493ed6d7cc4f324b0bb5bb
Jai Dev <47059807+youngsterjaidev@users.noreply.github.com>
2024-01-19 05:37:28 +0000

    make changes

--- lab/views.py
+++ lab/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_protect
 
 # adding the list
 tempData = []
 
 # Create your views here.
+@csrf_exempt
 def Register(request):
     if request.method == "POST":
         my_name = request.POST.get("name")
@@ -18,9 +20,10 @@ def Register(request):
             "phone": my_phone
         })
 
-        return JsonResponse({ message: "Data stored successfully !" }, safe=False)
+        return JsonResponse({ "message": "Data stored successfully !" }, safe=False)
     
     return JsonResponse({ "message": "All set !" }, safe=False)
 
+@csrf_exempt
 def index(request):
     return JsonResponse(tempData, safe=False)
